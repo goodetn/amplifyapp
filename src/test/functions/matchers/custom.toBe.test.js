a037360948ca74751cdeacda28e770576e629177
tottran <tottianddona@gmail.com>
2022-09-15 11:28:28 +0700

    Create custom.toBe.test.js

---
+++ src/test/functions/matchers/custom.toBe.test.js
@@ -0,0 +1,36 @@
+import { diff } from 'jest-diff';
+
+expect.extend({
+  toBe(received, expected) {
+    const options = {
+      comment: 'Object.is equality',
+      isNot: this.isNot,
+      promise: this.promise,
+    };
+
+    const pass = Object.is(received, expected);
+
+    const message = pass ?
+      () => this.utils.matcherHint('toBe', undefined, undefined, options) +
+        '\n\n' +
+        `Expected: not ${this.utils.printExpected(expected)}\n` +
+        `Received: not ${this.utils.printReceived(received)}\n`
+      :
+      () => {
+        const diffString = diff(expected, received, {
+          expand: this.expand,
+        });
+
+        return (
+          this.utils.matcherHint('toBe', undefined, undefined, options) +
+            '\n\n' +
+            (diffString && diffString.includes('- Expect'))
+            ? `Difference:\n\n${diffString}`
+            : `Expected: ${this.utils.printExpected(expected)}\n` +
+            `Received: ${this.utils.printReceived(received)}`
+        );
+      };
+    
+    return { actual: received, message, pass };
+  },
+});
