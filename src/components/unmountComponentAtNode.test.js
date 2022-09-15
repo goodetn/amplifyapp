d188d9a3face4e502143bdff998a5ac62f52137e
tottran <tottianddona@gmail.com>
2022-09-15 16:05:09 +0700

    Create unmountComponentAtNode.test.js

---
+++ src/components/unmountComponentAtNode.test.js
@@ -0,0 +1,21 @@
+import { unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+describe('unmountComponentAtNode describe', () => {
+});
+
+test('unmountComponentAtNode test', () => {
+});
