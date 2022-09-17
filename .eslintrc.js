8058d6d3b742bf40060a4a3144064aaaa7e193d1
tottran <tottianddona@gmail.com>
2022-09-17 19:25:42 +0700

    ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["react"],
+  rules: {
+    // I usually turn off these rules out of personal, feel free to delete the rules section in your project
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/prop-types": "off"
+  }
+};
